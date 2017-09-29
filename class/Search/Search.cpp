d704e8d27e58c32dbd3399deeaec11913965f302
Vinicius <vinicius.alves@poli.ufrj.br>
2017-09-29 10:30:40 -0300

    Otimização na soma de matrizes utilizando iterator

--- class/Search/Search.cpp
+++ class/Search/Search.cpp
@@ -16,18 +16,25 @@ void Search::calcAverageDistance(){
 	unsigned int j;
 	double sum = 0;
 	GraphTree *graph;
+	vector<double>::iterator it;
 
-#pragma omp parallel private(i,j,distance,graph) reduction(+:sum)
+#pragma omp parallel private(i,j,distance,graph,it) reduction(+:sum)
 {
 	#pragma omp for nowait schedule(dynamic)
 	for(i =0; i<totalVertexes-1; i++){
 
 		graph = this->dijkstra(i);
 		distance = graph->getDistance();
-		for (j =i+1; j<totalVertexes; j++){
-			if((*distance)[j]<infinityDouble)
-				sum+= (*distance)[j];
+		
+		it = distance->begin();
+		advance(it,i+1);
+
+		for (it; it!=distance->end(); ++it){
+
+			if(*it<infinityDouble)
+				sum+= *it;
 		} 
+
 		distance->clear();
 		delete graph;
 	}
